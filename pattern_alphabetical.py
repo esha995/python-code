e3c03098a4aba100be20fe90ff37a6739ae453cb
ESHA AGARWAL <80843306+esha995@users.noreply.github.com>
2021-03-19 20:51:08 +0530

    Add files via upload

--- pattern_alphabetical.py
+++ pattern_alphabetical.py
@@ -1,7 +1,7 @@
 n=eval(input("enter the no. of lines"))
+ch=65
 for i in range(1,n+1):
-    ch=65
-    for j in range(1,i+1):
+     for j in range(1,i+1):
         print(chr(ch),end=' ')
         ch+=1
-    print()
+     print()
