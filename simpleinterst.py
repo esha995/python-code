9e1ffb46cc2b786996de8a6242076987cc7c8cd1
ESHA AGARWAL <80843306+esha995@users.noreply.github.com>
2021-03-26 17:33:23 +0530

    Add files via upload

---
+++ simpleinterst.py
@@ -0,0 +1,5 @@
+P = int(input("Enter the principal amount : "))
+N = int(input("Enter the number of years : "))
+R = int(input("Enter the rate of interest : "))
+SI = (P * N * R)/100
+print("Simple interest :{}",format(SI))
